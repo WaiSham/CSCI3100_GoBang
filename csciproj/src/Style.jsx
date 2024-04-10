a45a3ebef12f0d11f325d28c4ad975713e01829f
ysulchaum <67533158+ysulchaum@users.noreply.github.com>
2024-04-10 23:51:18 +0800

    Add files via upload

--- csciproj/src/Style.jsx
+++ csciproj/src/Style.jsx
@@ -1,31 +1,49 @@
-import styled from "styled-components";
+//import styled components from react js
+import styled, { createGlobalStyle } from "styled-components";
+
 //style script
 
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: #121a25;
+    color: #ffffff;
+  }
+`;
 
+// Title section ------------------------------
 export const Title = styled.h1`
-  color: #333;
+  color: #fff;
   text-align: center;
 `;
 
 // Left Column --------------------------------
 export const LeftColumn = styled.div`
+  background-color: #0a1425;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
-  padding: 20px;
+  //padding-left: 20px;
+  flex: 1;
 `;
 
 export const Logo = styled.img`
-  width: 100px;
-  height: 100px;
+  // top: 2%;
+  // left: 3.5%;
+  // position: relative; 
+  width: 20vw;
+  height: 25vh;
   margin-bottom: 20px;
 `;
 
 export const LoginSection = styled.div`
+  background-color: #ccffcc;
+  color: black;
   display: flex;
   flex-direction: column;
   margin-bottom: 20px;
+  padding: 20px;
+  width: 20vw;
 `;
 
 export const UsernameInput = styled.input`
@@ -39,39 +57,76 @@ export const PasswordInput = styled.input`
 `;
 
 export const LoginButton = styled.button`
-  padding: 10px 20px;
-  background-color: #333;
-  color: white;
-  border: none;
-  cursor: pointer;
+  width: 100%;
+  height: 40px;
+  font-size: 16px;
+  margin-bottom: 10px;
 `;
 
-export const SignupButton = styled.button`
-  padding: 10px 20px;
-  background-color: #333;
-  color: white;
-  border: none;
-  cursor: pointer;
-`;
+// export const SignupButton = styled.button` //have a re-rendered sign up section
+//   width: 100%;
+//   height: 40px;
+//   font-size: 16px;
+//   margin-bottom: 10px;
+// `;
 
 export const GameModeSelection = styled.div`
+  background-color: #ccffcc;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin-bottom: 20px;
+  padding: 20px;
+  width: 20vw;
 `;
 
 export const GameModeButton = styled.button`
-  padding: 10px 20px;
-  margin-right: 10px;
-  background-color: #333;
-  color: white;
-  border: none;
-  cursor: pointer;
+  width: 200px;
+  height: 50px;
+  margin-bottom: 10px;
 `;
 
 export const FriendsList = styled.div`
-  margin-bottom: 20px;
+  background-color: #ccffcc;
+  color: black;
+  margin-bottom: 0px;
+  padding: 20px;
+  width: 20vw;
+  overflow-y: scroll;
+`;
+// Center Column (Signup Page) ---------------------
+
+export const SignForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-// Chess board ---------------------
+export const SignupTitle = styled.h2`
+  margin-bottom: 1rem;
+`;
+
+export const SignupInput = styled.input`
+  width: 100%;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+`;
+
+export const SignupButton = styled.button`
+  padding: 0.5rem 1rem;
+`;
+
+// Center Column (Chess board) ---------------------
+export const CenterColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0px 0px 0px;
+  flex: 1;
+`;
+
 export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 3fr 1fr;
@@ -82,24 +137,27 @@ export const ChessContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 0;
 `;
 
 export const Checkerboard = styled.div`
   display: inline-block;
-  margin-top: 0;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Row = styled.div`
   display: flex;
 `;
 
+//For the pop up banner and such
 export const WinnerModal = styled.div`
   position: fixed;
   top: 0;
   right: 0;
   left: 0;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.8);
+  background: RGBA(0,0,0,0.8);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -107,7 +165,8 @@ export const WinnerModal = styled.div`
 `;
 
 export const ModalInner = styled.div`
-  background: white;
+  background: #ccffcc;
+  border-radius: 2%;
   color: black;
   height: 300px;
   width: 300px;
@@ -115,48 +174,123 @@ export const ModalInner = styled.div`
   text-align: center;
 `;
 
-// Right Column ------------------------------- 
+export const ModalText =styled.div`
+  padding: 50px 0px 50px 0px;
+  border-radius: 10%;
+  border: solid #000 1px;
+  background: #eee;
+  color: Black;
+  width: 300px;
+  text-align: center;
+  color: black;
+`;
+
+//to contain button
+export const ModalInnerInner =styled.div`
+  width: 19vw;
+`;
+
+//to contain text
+export const ModalInnerInner2 =styled.div`
+  height: 50%;
+  width: 300px;
+  text-align: center;
+  color: black;
+`;
+
+export const ModalButton = styled.button`
+  /* Styles for the chat send button */
+  // flex-direction: column;
+
+  text-align: center;
+  border-radius: 10%;
+  background-color: #ccc;
+  width: 150px;
+  height: 70px;
+  color: black;
+  border: solid #000 1px;
+  cursor: pointer;
+  margin-bottom: 20px;
+`;
+
+// Right Column -------------------------------
 export const RightColumn = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  padding: 20px;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 20px 0px 0px;
+  flex: 1;
 `;
 
 export const Timer = styled.div`
+  background-color: #ccffcc;
+  color: black;
   margin-bottom: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  width: 20vw;
 `;
 
 export const GameInfo = styled.div`
+  background-color: #ccffcc;
+  color: black;
   margin-bottom: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  width: 20vw;
+`;
+
+export const GameControl = styled.div`
+  background-color: #ccffcc;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+  padding: 20px;
+  width: 20vw;
 `;
 
 export const GameControlButton = styled.button`
   padding: 10px 20px;
-  background-color: #333;
-  color: white;
+  background-color: #ccc;
+  width: 200px;
+  height: 50px;
+  color: black;
   border: none;
   cursor: pointer;
   margin-bottom: 20px;
 `;
 
 export const ChatBox = styled.div`
-  width: 100%;
+  background-color: #ccffcc;
+  color: black;
+  width: 20vw;
   height: 200px;
   border: 1px solid #ccc;
-  padding: 10px;
+  padding: 20px;
   overflow-y: scroll;
+  // display: flex;
+  flex-direction: column;
 `;
 
 export const ChatMessages = styled.div`
   /* Styles for the chat messages container */
+  height: 95%;
+  text-align: left;
 `;
 
 export const ChatInput = styled.input`
   /* Styles for the chat input box */
+  width: 200px;
+  
+  // horizontal-align: center;
+  align-self: flex-end;
 `;
 
 export const ChatButton = styled.button`
   /* Styles for the chat send button */
+  // flex-direction: column;
+  align-self: flex-end;
 `;
